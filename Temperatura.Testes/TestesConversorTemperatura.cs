39b74f7337a0819b429d8719bfc94d7718c216a5
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-01 20:04:03 -0300

    .NET Core 3.1 + .NET Standard + Unit Testing + xUnit
    .NET Core 3.1 + .NET Standard + Unit Testing + xUnit

---
+++ Temperatura.Testes/TestesConversorTemperatura.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace Temperatura.Testes
+{
+    public class TestesConversorTemperatura
+    {
+        [Theory]
+        [InlineData(32, 0)]
+        [InlineData(86, 30)]
+        [InlineData(90.5, 32.5)]
+        [InlineData(212, 100)]
+        public void TestarConversaoTemperatura(
+            double fahrenheit, double celsius)
+        {
+            double valorCalculado =
+                ConversorTemperatura.FahrenheitParaCelsius(fahrenheit);
+            Assert.Equal(celsius, valorCalculado);
+        }
+    }
+}
